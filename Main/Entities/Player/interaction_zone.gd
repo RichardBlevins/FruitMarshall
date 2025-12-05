ee056f58d3ccaf01e0998be2590a9862b1a92e19
RichardBlevins <ngoodmonster@gmail.com>
2025-12-04 23:43:57 -0800

    fixed collision with savepoints and deleted a couple things

--- Main/Entities/Player/interaction_zone.gd
+++ Main/Entities/Player/interaction_zone.gd
@@ -1,15 +1,16 @@
 extends Area2D
 
-
 func _input(event):
-	if event.is_action_pressed("ui_up"):  # Or your interact key
+	if event.is_action_pressed("ui_up"):  
+		print("saved")
 		var overlapping = get_overlapping_bodies()
-		if overlapping.size() > 0 and overlapping[0].is_in_group("player"):
+		if overlapping.size() > 0 and overlapping[0].is_in_group("savepoint"):
 			overlapping[0].save_player_data()
+			
 
 
 func _on_area_entered(area: Area2D) -> void:
 	print("saved")
-	if area.is_in_group("player"):
+	if area.is_in_group("savepoint"):
 		# Show save prompt
 		print("Press the up arrow to save")
